494112f666dd7c9fec03b6abba5c1c8b15b764b0
Cydnirn <senoaji0003@gmail.com>
2023-03-29 11:11:53 +0700

    Add Comment

--- ServoSmoot.ino
+++ ServoSmoot.ino
@@ -1,13 +1,22 @@
 #include <Servo.h>
-Servo ZaHando;
+Servo ZaHando; //Object Servo
 int endVal = 0, signal = 0;
+
+/*
+  trigPin = Pin mengirim suara ultrasonik
+  echoPin = Pin menerima suara ultrasonik
+  maxDistanceToScan = Jarak maksimal agar pintu terbuka
+*/
+
 const int trigPin = 9, echoPin = 10, maxDistanceToScan = 40;
 
+//Fungsi untuk menggerakan servo
+// int& => integer yang mereferensikan variabel asli, saat diubah maka variabel asli juga berubah
 void SmoothBore(Servo X, int maxInt, int increment, int& currentVal, int delayTime = 500) {
   if (maxInt < 0 || maxInt > 180) {
     Serial.println("Not valid maximum integer");
   } else if (currentVal < maxInt) {
-    if (currentVal == maxInt)
+    if (currentVal == maxInt) 
       ;
     else {
       for (int i = 0; i <= maxInt; i += increment) {
@@ -17,6 +26,7 @@ void SmoothBore(Servo X, int maxInt, int increment, int& currentVal, int delayTi
       currentVal = maxInt;
     }
   }
+  //Mengurangi / Menutup
   else if (currentVal >= maxInt){
     if(currentVal == maxInt);
     else{
@@ -29,12 +39,14 @@ void SmoothBore(Servo X, int maxInt, int increment, int& currentVal, int delayTi
   }
 }
 
+//Menghitung jarak berdasarkan delay dari echo dan trigger
 int CalcDistance(int echo) {
   long duration;
   duration = pulseIn(echo, 1);
   return duration / 58.2;
 }
 
+//Fungsi yang mengeksekusi penghitungan jarak
 int DistanceUltra(int trigger, int echo) {
   int delay = 2, delayAtt = 10;
 
